bdc727f85d371fd35d92a197ced673a059a11664
Joao Paulo Marques <joaopaulo_marquesj@outlook.com>
2021-08-02 22:06:07 -0300

    Projeto BX, layout MEnu

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace PROJETOBX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelEbex = new System.Windows.Forms.Panel();
+            this.labelEbex = new System.Windows.Forms.Label();
+            this.pictureBoxPainel = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCadastros = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProtocolos = new System.Windows.Forms.PictureBox();
+            this.pictureBoxListas = new System.Windows.Forms.PictureBox();
+            this.btnPainel = new System.Windows.Forms.Button();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.btnProtocolos = new System.Windows.Forms.Button();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnListas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxBarraSup = new System.Windows.Forms.PictureBox();
+            this.panelOpcoesJanela = new System.Windows.Forms.Panel();
+            this.buttonFechar = new System.Windows.Forms.Button();
+            this.buttonMaximizar = new System.Windows.Forms.Button();
+            this.buttonMinimizar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            this.panelEbex.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPainel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCadastros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProtocolos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxListas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBarraSup)).BeginInit();
+            this.panelOpcoesJanela.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelMenu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelMenu.BackgroundImage")));
+            this.panelMenu.Controls.Add(this.panelEbex);
+            this.panelMenu.Controls.Add(this.pictureBoxPainel);
+            this.panelMenu.Controls.Add(this.pictureBoxCadastros);
+            this.panelMenu.Controls.Add(this.pictureBoxProtocolos);
+            this.panelMenu.Controls.Add(this.pictureBoxListas);
+            this.panelMenu.Controls.Add(this.btnPainel);
+            this.panelMenu.Controls.Add(this.buttonHelp);
+            this.panelMenu.Controls.Add(this.btnProtocolos);
+            this.panelMenu.Controls.Add(this.btnCadastro);
+            this.panelMenu.Controls.Add(this.btnListas);
+            this.panelMenu.Location = new System.Drawing.Point(-1, -1);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(239, 451);
+            this.panelMenu.TabIndex = 2;
+            // 
+            // panelEbex
+            // 
+            this.panelEbex.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelEbex.BackgroundImage")));
+            this.panelEbex.Controls.Add(this.labelEbex);
+            this.panelEbex.Location = new System.Drawing.Point(0, 0);
+            this.panelEbex.Name = "panelEbex";
+            this.panelEbex.Size = new System.Drawing.Size(239, 50);
+            this.panelEbex.TabIndex = 15;
+            // 
+            // labelEbex
+            // 
+            this.labelEbex.AutoSize = true;
+            this.labelEbex.BackColor = System.Drawing.Color.Transparent;
+            this.labelEbex.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelEbex.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelEbex.Location = new System.Drawing.Point(2, 10);
+            this.labelEbex.Name = "labelEbex";
+            this.labelEbex.Size = new System.Drawing.Size(219, 21);
+            this.labelEbex.TabIndex = 0;
+            this.labelEbex.Text = "EBEX - ENTREGAS EXPRESSAS";
+            // 
+            // pictureBoxPainel
+            // 
+            this.pictureBoxPainel.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxPainel.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPainel.Image")));
+            this.pictureBoxPainel.Location = new System.Drawing.Point(6, 216);
+            this.pictureBoxPainel.Name = "pictureBoxPainel";
+            this.pictureBoxPainel.Size = new System.Drawing.Size(50, 39);
+            this.pictureBoxPainel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxPainel.TabIndex = 14;
+            this.pictureBoxPainel.TabStop = false;
+            // 
+            // pictureBoxCadastros
+            // 
+            this.pictureBoxCadastros.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxCadastros.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxCadastros.Image")));
+            this.pictureBoxCadastros.Location = new System.Drawing.Point(1, 279);
+            this.pictureBoxCadastros.Name = "pictureBoxCadastros";
+            this.pictureBoxCadastros.Size = new System.Drawing.Size(50, 39);
+            this.pictureBoxCadastros.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxCadastros.TabIndex = 12;
+            this.pictureBoxCadastros.TabStop = false;
+            // 
+            // pictureBoxProtocolos
+            // 
+            this.pictureBoxProtocolos.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxProtocolos.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxProtocolos.Image")));
+            this.pictureBoxProtocolos.Location = new System.Drawing.Point(4, 152);
+            this.pictureBoxProtocolos.Name = "pictureBoxProtocolos";
+            this.pictureBoxProtocolos.Size = new System.Drawing.Size(50, 39);
+            this.pictureBoxProtocolos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxProtocolos.TabIndex = 11;
+            this.pictureBoxProtocolos.TabStop = false;
+            this.pictureBoxProtocolos.Click += new System.EventHandler(this.pictureBoxProtocolos_Click);
+            // 
+            // pictureBoxListas
+            // 
+            this.pictureBoxListas.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxListas.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxListas.Image")));
+            this.pictureBoxListas.Location = new System.Drawing.Point(0, 87);
+            this.pictureBoxListas.Name = "pictureBoxListas";
+            this.pictureBoxListas.Size = new System.Drawing.Size(50, 39);
+            this.pictureBoxListas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxListas.TabIndex = 10;
+            this.pictureBoxListas.TabStop = false;
+            this.pictureBoxListas.Click += new System.EventHandler(this.pictureBoxListas_Click);
+            // 
+            // btnPainel
+            // 
+            this.btnPainel.BackColor = System.Drawing.Color.Transparent;
+            this.btnPainel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPainel.Location = new System.Drawing.Point(0, 206);
+            this.btnPainel.Name = "btnPainel";
+            this.btnPainel.Size = new System.Drawing.Size(199, 58);
+            this.btnPainel.TabIndex = 2;
+            this.btnPainel.Text = "Painel";
+            this.btnPainel.UseVisualStyleBackColor = false;
+            this.btnPainel.Click += new System.EventHandler(this.btnPainel_Click);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonHelp.Location = new System.Drawing.Point(13, 425);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(199, 23);
+            this.buttonHelp.TabIndex = 8;
+            this.buttonHelp.Text = "Peça ajuda ao desenvolvedor";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnProtocolos
+            // 
+            this.btnProtocolos.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnProtocolos.Location = new System.Drawing.Point(0, 142);
+            this.btnProtocolos.Name = "btnProtocolos";
+            this.btnProtocolos.Size = new System.Drawing.Size(199, 58);
+            this.btnProtocolos.TabIndex = 7;
+            this.btnProtocolos.Text = "Protocolos";
+            this.btnProtocolos.UseVisualStyleBackColor = true;
+            this.btnProtocolos.Click += new System.EventHandler(this.btnProtocolos_Click);
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCadastro.Location = new System.Drawing.Point(0, 270);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(199, 58);
+            this.btnCadastro.TabIndex = 1;
+            this.btnCadastro.Text = "Cadastros";
+            this.btnCadastro.UseVisualStyleBackColor = true;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // btnListas
+            // 
+            this.btnListas.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnListas.Location = new System.Drawing.Point(0, 78);
+            this.btnListas.Name = "btnListas";
+            this.btnListas.Size = new System.Drawing.Size(199, 58);
+            this.btnListas.TabIndex = 4;
+            this.btnListas.Text = "Listas";
+            this.btnListas.UseVisualStyleBackColor = true;
+            this.btnListas.Click += new System.EventHandler(this.btnListas_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(670, 426);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Versão de teste: 0.001";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBoxBarraSup
+            // 
+            this.pictureBoxBarraSup.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxBarraSup.BackgroundImage")));
+            this.pictureBoxBarraSup.Location = new System.Drawing.Point(238, -1);
+            this.pictureBoxBarraSup.Name = "pictureBoxBarraSup";
+            this.pictureBoxBarraSup.Size = new System.Drawing.Size(562, 50);
+            this.pictureBoxBarraSup.TabIndex = 10;
+            this.pictureBoxBarraSup.TabStop = false;
+            // 
+            // panelOpcoesJanela
+            // 
+            this.panelOpcoesJanela.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelOpcoesJanela.BackgroundImage")));
+            this.panelOpcoesJanela.Controls.Add(this.buttonFechar);
+            this.panelOpcoesJanela.Controls.Add(this.buttonMaximizar);
+            this.panelOpcoesJanela.Controls.Add(this.buttonMinimizar);
+            this.panelOpcoesJanela.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelOpcoesJanela.Location = new System.Drawing.Point(686, -1);
+            this.panelOpcoesJanela.Name = "panelOpcoesJanela";
+            this.panelOpcoesJanela.Size = new System.Drawing.Size(114, 31);
+            this.panelOpcoesJanela.TabIndex = 11;
+            // 
+            // buttonFechar
+            // 
+            this.buttonFechar.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonFechar.Image = ((System.Drawing.Image)(resources.GetObject("buttonFechar.Image")));
+            this.buttonFechar.Location = new System.Drawing.Point(78, 2);
+            this.buttonFechar.Name = "buttonFechar";
+            this.buttonFechar.Size = new System.Drawing.Size(30, 26);
+            this.buttonFechar.TabIndex = 2;
+            this.buttonFechar.UseVisualStyleBackColor = true;
+            this.buttonFechar.Click += new System.EventHandler(this.buttonFechar_Click);
+            // 
+            // buttonMaximizar
+            // 
+            this.buttonMaximizar.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonMaximizar.Image = ((System.Drawing.Image)(resources.GetObject("buttonMaximizar.Image")));
+            this.buttonMaximizar.Location = new System.Drawing.Point(41, 2);
+            this.buttonMaximizar.Name = "buttonMaximizar";
+            this.buttonMaximizar.Size = new System.Drawing.Size(30, 26);
+            this.buttonMaximizar.TabIndex = 1;
+            this.buttonMaximizar.UseVisualStyleBackColor = true;
+            this.buttonMaximizar.Click += new System.EventHandler(this.buttonMaximizar_Click);
+            // 
+            // buttonMinimizar
+            // 
+            this.buttonMinimizar.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("buttonMinimizar.Image")));
+            this.buttonMinimizar.Location = new System.Drawing.Point(4, 2);
+            this.buttonMinimizar.Name = "buttonMinimizar";
+            this.buttonMinimizar.Size = new System.Drawing.Size(30, 26);
+            this.buttonMinimizar.TabIndex = 0;
+            this.buttonMinimizar.UseVisualStyleBackColor = true;
+            this.buttonMinimizar.Click += new System.EventHandler(this.buttonMinimizar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(324, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 303);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelOpcoesJanela);
+            this.Controls.Add(this.pictureBoxBarraSup);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelMenu.ResumeLayout(false);
+            this.panelEbex.ResumeLayout(false);
+            this.panelEbex.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPainel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCadastros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProtocolos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxListas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBarraSup)).EndInit();
+            this.panelOpcoesJanela.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnProtocolos;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnListas;
+        private System.Windows.Forms.Button btnPainel;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxCadastros;
+        private System.Windows.Forms.PictureBox pictureBoxProtocolos;
+        private System.Windows.Forms.PictureBox pictureBoxListas;
+        private System.Windows.Forms.PictureBox pictureBoxPainel;
+        private System.Windows.Forms.PictureBox pictureBoxBarraSup;
+        private System.Windows.Forms.Panel panelEbex;
+        private System.Windows.Forms.Label labelEbex;
+        private System.Windows.Forms.Panel panelOpcoesJanela;
+        private System.Windows.Forms.Button buttonFechar;
+        private System.Windows.Forms.Button buttonMaximizar;
+        private System.Windows.Forms.Button buttonMinimizar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJETOBX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //btns
+            btnPainel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnPainel.FlatAppearance.BorderSize = 0;
+            btnPainel.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnPainel.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnPainel.BackColor = Color.Transparent;
+
+            btnListas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnListas.FlatAppearance.BorderSize = 0;
+            btnListas.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnListas.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnListas.BackColor = Color.Transparent;
+
+            btnProtocolos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnProtocolos.FlatAppearance.BorderSize = 0;
+            btnProtocolos.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnProtocolos.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnProtocolos.BackColor = Color.Transparent;
+
+            btnCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnCadastro.FlatAppearance.BorderSize = 0;
+            btnCadastro.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnCadastro.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnCadastro.BackColor = Color.Transparent;
+
+            buttonHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            buttonHelp.FlatAppearance.BorderSize = 0;
+            buttonHelp.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            buttonHelp.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            buttonHelp.BackColor = Color.Transparent;
+
+            buttonMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            buttonMinimizar.FlatAppearance.BorderSize = 0;
+            buttonMinimizar.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            buttonMinimizar.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            buttonMinimizar.BackColor = Color.Transparent;
+
+            buttonFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            buttonFechar.FlatAppearance.BorderSize = 0;
+            buttonFechar.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            buttonFechar.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            buttonFechar.BackColor = Color.Transparent;
+
+            buttonMaximizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            buttonMaximizar.FlatAppearance.BorderSize = 0;
+            buttonMaximizar.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            buttonMaximizar.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            buttonMaximizar.BackColor = Color.Transparent;
+
+
+            //picturebox
+
+
+
+
+
+
+
+        }
+
+        private void buttonMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPainel_Click(object sender, EventArgs e)
+        {
+            Painel painel = new Painel();
+            painel.Show();
+            
+
+        }
+
+        private void btnListas_Click(object sender, EventArgs e)
+        {
+            Listas list = new Listas();
+            list.Show();
+        }
+
+        private void btnProtocolos_Click(object sender, EventArgs e)
+        {
+            Protocolos protocolar = new Protocolos();
+            protocolar.Show();
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            Cadastros cad = new Cadastros();
+            cad.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Help ajuda = new Help();
+            ajuda.Show();
+        }
+
+        private void pictureBoxProtocolos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxListas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+        }
+
+        private void buttonMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
